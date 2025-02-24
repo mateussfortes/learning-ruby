f865c02ed8f24cdf08c7f2048a779f85e8ec824f
Mateus <mateussilveirafortes@gmail.com>
2025-02-24 11:30:18 -0300

    Flow control

---
+++ 18-flow-control.rb
@@ -0,0 +1,28 @@
+if 10 < 10 then
+  print "10 is less than 20"
+end
+
+# after the print
+print "10 is less than 20" if 10 < 20
+
+# can use with semi colon
+if 10 < 20: print "10 is less than 20" end
+  
+# if else statement
+
+if customerName == "Fred"
+  print "Hello Fred"
+elsif customerName == "Jhon"
+  print "Hello Jhon"
+elsif customerName == "Sergio"
+  print "Sergio"
+end
+
+# using colon
+if customerName == "Fred" : print "Hello to fred"
+elsif customerName == "John" : print "Hello to fred"
+elsif customerName == "Mateus" : print "Hello to fred"
+end
+
+# ternary
+customerName == "Fred" ? "Hello Fred" : "Who are you?"
